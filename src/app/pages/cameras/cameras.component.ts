b6c933eaabd2917b392e46f0ba393c4969df18ed
Kiara Maldonado <maldonadogarciakiara@gmail.com>
2023-11-17 12:56:46 +0000

    CAMERAS: Hero and posts

--- src/app/pages/cameras/cameras.component.ts
+++ src/app/pages/cameras/cameras.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-cameras',
   templateUrl: './cameras.component.html',
   styleUrls: ['./cameras.component.css']
 })
-export class CamerasComponent {
+export class CamerasComponent implements OnInit {
+  cameraPosts = this.postService.getPostsByCategory('cameras');
 
+  constructor(private postService: PostService) { }
+
+  ngOnInit(): void {
+  }
 }
