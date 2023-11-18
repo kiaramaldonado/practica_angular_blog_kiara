ac3ff4aba4fcf9ba4ef8fb775bb914942c0f2424
Kiara Maldonado <maldonadogarciakiara@gmail.com>
2023-11-18 15:24:52 +0000

    ALL CATEGORY PAGES updated

--- src/app/pages/filmmaking/filmmaking.component.ts
+++ src/app/pages/filmmaking/filmmaking.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-filmmaking',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./filmmaking.component.css']
 })
 export class FilmmakingComponent {
+  filmPosts = this.postService.getPostsByCategory('filmmaking');
 
+  constructor(private postService: PostService) { }
+
+  ngOnInit(): void {
+  }
+
+  onFilterChanged(filterOptions: { sortOrder: string, searchTerm: string }) {
+    const { sortOrder, searchTerm } = filterOptions;
+
+    let filteredPosts = this.filmPosts;
+
+    if (sortOrder === 'newest') {
+      filteredPosts = filteredPosts.sort((a, b) => new Date(b.publishDate).getTime() - new Date(a.publishDate).getTime());
+    } else if (sortOrder === 'oldest') {
+      filteredPosts = filteredPosts.sort((a, b) => new Date(a.publishDate).getTime() - new Date(b.publishDate).getTime());
+    }
+
+    if (searchTerm.trim() !== '') {
+      filteredPosts = filteredPosts.filter(post =>
+        post.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        post.excerpt.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    }
+
+    this.filmPosts = filteredPosts;
+  }
 }
--- src/app/pages/photography/photography.component.ts
+++ src/app/pages/photography/photography.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-photography',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class PhotographyComponent {
 
+  photographyPosts = this.postService.getPostsByCategory('photography');
+
+  constructor(private postService: PostService) { }
+
+  ngOnInit(): void {
+  }
+
+  onFilterChanged(filterOptions: { sortOrder: string, searchTerm: string }) {
+    const { sortOrder, searchTerm } = filterOptions;
+
+    let filteredPosts = this.photographyPosts;
+
+    if (sortOrder === 'newest') {
+      filteredPosts = filteredPosts.sort((a, b) => new Date(b.publishDate).getTime() - new Date(a.publishDate).getTime());
+    } else if (sortOrder === 'oldest') {
+      filteredPosts = filteredPosts.sort((a, b) => new Date(a.publishDate).getTime() - new Date(b.publishDate).getTime());
+    }
+
+    if (searchTerm.trim() !== '') {
+      filteredPosts = filteredPosts.filter(post =>
+        post.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        post.excerpt.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    }
+
+    this.photographyPosts = filteredPosts;
+  }
 }
