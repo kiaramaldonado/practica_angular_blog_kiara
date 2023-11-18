83c90b698be45ceb17edf66a6da7f837aecf0375
Kiara Maldonado <maldonadogarciakiara@gmail.com>
2023-11-18 11:24:55 +0000

    LENSES: Hero and posts

--- src/app/pages/lenses/lenses.component.ts
+++ src/app/pages/lenses/lenses.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-lenses',
   templateUrl: './lenses.component.html',
   styleUrls: ['./lenses.component.css']
 })
-export class LensesComponent {
+export class LensesComponent implements OnInit {
+  lensePosts = this.postService.getPostsByCategory('lenses');
 
+  constructor(private postService: PostService) { }
+
+  ngOnInit(): void {
+  }
 }
